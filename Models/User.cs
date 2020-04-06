e4dfa6f43523659a40e74747e21f78fa62f58f35
Austin <austingpowel@gmail.com>
2020-04-06 14:41:18 -0400

    altered name regex to allow for space

--- Models/User.cs
+++ Models/User.cs
@@ -13,7 +13,7 @@ namespace Lab21.Models
         public int Id { get; set; }
         [Required(ErrorMessage = "A name is required.")]
         [StringLength(20, ErrorMessage = "UserName must be between 3 and 10 characters")]
-        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "Numbers and symbols are not allowed in a name.")]
+        [RegularExpression(@"^[a-zA-Z|\s]+$", ErrorMessage = "Numbers and symbols are not allowed in a name.")]   //will this regex allow extra spaces at the beginning or end of a name?
         public string Name { get; set; }
         [Required(ErrorMessage = "An email is required.")]
         [EmailAddress(ErrorMessage = "That is not a valid email format.")]
